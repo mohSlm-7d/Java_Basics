53f688d7987dee27b34ca84d519e637b049be2e3
mohSlm-7d <68254871+mohSlm-7d@users.noreply.github.com>
2023-04-09 03:01:29 +0300

    Add files via upload

---
+++ GUI Apps/CalcApp/src/calcapp/CalcApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcapp;
+import javax.swing.*;
+/**
+ *
+ * @author M7md_2
+ */
+public class CalcApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CalcFrame Calc=new CalcFrame();
+        
+    }
+    
+}
